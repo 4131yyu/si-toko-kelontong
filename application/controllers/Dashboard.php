de9f834bd728a9a4a1ba73a404fff36edbeca182
Amru <bloodmon136@gmail.com>
2026-06-19 21:34:34 +0800

    membuat dashboard tentang datanya dll

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,25 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+        if (!$this->session->userdata('logged_in')) redirect('Auth');
+        $this->load->model(['Produk', 'Transaksi', 'Kategori', 'User']);
+    }
+
+    public function index(){
+        $data = [
+        'title'             =>'Dashboard',
+        'total_transaksi'   =>$this->Transaksi->total_hari_ini(),
+        'omzet_hari_ini'    =>$this->Transaksi->omzet_hari_ini(),
+        'total_produk'      =>$this->Produk->count_all(),
+        'total_kategori'    =>$this->Kategori->count_all(),
+        'stok_menipis'      =>$this->Produk->stok_menipis(10),
+        'produk_terlaris'   =>$this->Produk->terlaris(5),
+        ];
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('dashboard/index', $data);
+        $this->load->view('templates/footer');
+    }
+}
