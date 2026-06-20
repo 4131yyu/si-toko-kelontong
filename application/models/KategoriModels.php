669952019322c7c7fda01d1ddccbedf41e845476
Amru <bloodmon136@gmail.com>
2026-06-20 11:35:58 +0800

    tambahkan kategorimodels dan isi coding dari phpnya

---
+++ application/models/KategoriModels.php
@@ -0,0 +1,30 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class KategoriModels extends CI_Model {
+
+    public function get_all() {
+        return $this->db->get('kategori')->result();
+    }
+
+    public function get_by_id($id) {
+        return $this->db->get_where('kategori', ['id_kategori' => $id])->row();
+    }
+
+    public function insert($data) {
+        return $this->db->insert('kategori', $data);
+    }
+
+    public function update($id, $data) {
+        $this->db->where('id_kategori', $id);
+        return $this->db->update('kategori', $data);
+    }
+
+    public function delete($id) {
+        $this->db->where('id_kategori', $id);
+        return $this->db->delete('kategori');
+    }
+
+    public function count_all() {
+        return $this->db->count_all('kategori');
+    }
+}
