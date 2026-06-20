a83e41226a7007317c2c90fbc6aa6c6fff2a8281
Feby <fbyy05@gmail.com>
2026-06-20 22:20:20 +0800

    menambahkan paragraf transaksi hari ini

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -3,6 +3,9 @@
         <div class="card text-white bg-primary shadow-sm">
             <div class="card-body">
                 <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <p class="mb-0 small">Transaksi Hari Ini</p>
+                    </div>
 
                 </div>
 
