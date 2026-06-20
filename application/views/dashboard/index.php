7ee4fd52c2439d4fca13c50f6bbf3caeb8b6c156
Feby <fbyy05@gmail.com>
2026-06-20 23:17:22 +0800

    menambahkan caard body

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -14,6 +14,9 @@
             </div>
             <div class="col-md-3">
                 <div class="card text-white bg-success shadow-sm">
+                    <div class="card-body">
+
+                    </div>
 
                 </div>
 
