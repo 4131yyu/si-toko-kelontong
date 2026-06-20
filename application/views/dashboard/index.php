bd8ed447e17778c92bd9763dfc54172d0d06420d
Feby <fbyy05@gmail.com>
2026-06-20 22:14:51 +0800

    mengganti bg-secondary

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,6 +1,9 @@
 <div  class="row g-3 mb-4">
     <div class="col-md-3">
-        <div class="card text-white bg-primary shadow-sm">
+        <div class="card text-white bg-secondary shadow-sm">
+            <div >
+
+            </div>
 
         </div>
     </div>
