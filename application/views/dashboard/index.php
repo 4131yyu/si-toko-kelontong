b3f1c8b36151e155fc70ed223f90417b50d077fc
Feby <fbyy05@gmail.com>
2026-06-20 22:18:40 +0800

    mengganti bg pada bootstrap

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,6 +1,6 @@
 <div  class="row g-3 mb-4">
     <div class="col-md-3">
-        <div class="card text-white bg-secondary shadow-sm">
+        <div class="card text-white bg-primary shadow-sm">
             <div class="card-body">
                 <div class="d-flex justify-content-between align-items-center">
 
