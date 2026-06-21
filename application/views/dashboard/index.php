da919e1033b0bce0f7246e0aee1831321fba5794
Feby <fbyy05@gmail.com>
2026-06-21 13:21:15 +0800

    menambahkan card body

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -40,6 +40,9 @@
             </div>
             <div class="col-md-3">
                 <div class="card text-white bg-secondary shadow-sm">
+                    <div class="card-body">
+                        
+                    </div>
 
                 </div>
 
