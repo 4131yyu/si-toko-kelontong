2fbe07371a5dd8d6a4084b5254cc7af2e106e80e
Amru <bloodmon136@gmail.com>
2026-06-21 21:27:17 +0800

    memperbaiki beberapa kalimat yang typo dan blom disesuaikan

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,7 +4,7 @@ class Dashboard extends CI_Controller{
     public function __construct(){
         parent::__construct();
         if (!$this->session->userdata('logged_in')) redirect('Auth');
-        $this->load->model(['Produk', 'Transaksi', 'Kategori', 'User']);
+        $this->load->model(['ProdukModels', 'TransaksiModels', 'KategoriModels', 'UserModels']);
     }
 
     public function index(){
