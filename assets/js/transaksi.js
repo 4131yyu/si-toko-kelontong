697a665a09baeebd13b6431b6f5be8284d5690b6
Abiyyu <abiyyu18@gmail.com>
2026-06-21 20:18:21 +0800

    feat: update pilih produk

--- assets/js/transaksi.js
+++ assets/js/transaksi.js
@@ -7,4 +7,29 @@ $(function () {
     function parseRupiah(text) {
         return Number(String(text || '0').replace(/[^0-9]/g, '')) || 0;
     }
+
+    function updateRow(row) {
+        var $row = $(row);
+        var $select = $row.find('.select-produk');
+        var $option = $select.find('option:selected');
+
+        var harga = Number($option.data('harga')) || 0;
+        var stok = Number($option.data('stok')) || 0;
+        var $jumlahInput = $row.find('.input-jumlah');
+        var jumlah = parseInt($jumlahInput.val()) || 0;
+
+        // Validasi: jumlah tidak boleh melebihi stok yang tersedia
+        if (stok > 0 && jumlah > stok) {
+            jumlah = stok;
+            $jumlahInput.val(stok);
+        }
+
+        var subtotal = harga * jumlah;
+
+        $row.find('.stok-cell').text($select.val() !== '' ? stok : '-');
+        $row.find('.harga-cell').text(formatRupiah(harga));
+        $row.find('.subtotal-cell').text(formatRupiah(subtotal));
+
+        return subtotal;
+    }
 })
