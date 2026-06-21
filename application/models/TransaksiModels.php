584c748371c14dfd3aad970d0ffe46edf7b8c8f2
Amru <bloodmon136@gmail.com>
2026-06-21 21:00:45 +0800

    memassukan function get by id

--- application/models/TransaksiModels.php
+++ application/models/TransaksiModels.php
@@ -9,4 +9,11 @@ class TransaksiModels extends CI_Model {
         $this->db->order_by('transaksi.tgl_transaksi', 'DESC');
         return $this->db->get()->result();
     }
+    public function get_by_id($id) {
+        $this->db->select('transaksi.*, users.nama_lengkap');
+        $this->db->from('transaksi');
+        $this->db->join('users', 'users.id_user = transaksi.id_user');
+        $this->db->where('transaksi.id_transaksi', $id);
+        return $this->db->get()->row();
+    }
 }
