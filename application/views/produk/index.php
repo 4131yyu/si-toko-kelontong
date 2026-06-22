3e1f32ee169c757189dcc391ad7083c5ae1b0525
Abiyyu <abiyyu18@gmail.com>
2026-06-22 10:16:46 +0800

    feat: tambah tampilan table produk

--- application/views/produk/index.php
+++ application/views/produk/index.php
@@ -5,4 +5,19 @@
             <i class="bi bi-plus-lg"></i>
         </a>
     </div>
+    <div class="card-body">
+        <table class="table table-hover align-middle">
+            <thead class="table-light">
+                <tr>
+                    <th style="width:50px;">No</th>
+                    <th>Nama Produk</th>
+                    <th>Kategori</th>
+                    <th class="text-end">Stok</th>
+                    <th class="text-end">Harga Beli</th>
+                    <th class="text-end">Harga Jual</th>
+                    <th style="width:140px;" class="text-center">Aksi</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
 </div>
