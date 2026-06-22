62713b4248a8d85471c7fa191138d0c72abce5d8
Abiyyu <abiyyu18@gmail.com>
2026-06-22 10:13:42 +0800

    feat: tambah view produk

---
+++ application/views/produk/index.php
@@ -0,0 +1,8 @@
+<div class="card shadow-sm">
+    <div class="card-header bg-white d-flex justify-content-between align-items-center">
+        <span>Daftar Produk</span>
+        <a href="<?= base_url('Produk/tambah') ?>" class="btn btn-primary btn-sm">
+            <i class="bi bi-plus-lg"></i>
+        </a>
+    </div>
+</div>
