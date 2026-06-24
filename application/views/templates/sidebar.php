396c138f6384d3b4149c04d81701d805685c39b7
Feby <fbyy05@gmail.com>
2026-06-25 01:32:11 +0800

    Menambahkan fungsi untuk menentukan menu sidebar yang aktif

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -2,3 +2,7 @@
 $role = $this->session->userdata('role');
 $current_controller = strtolower($this->uri->segment(1) ?: 'dashboard');
 $current_method = strtolower($this->uri->segment(2) ?: 'index');
+
+if (!function_exists('sidebar_active')) {
+    function sidebar_active($controller, $method = null)
+    {
