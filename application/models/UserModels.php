f59710ad697e3c0b69d4a32f70c8f0882c457d63
Earlene <kimlenn427@gmail.com>
2026-06-24 22:50:00 +0800

    menambahkan class usermodels dan function get all

--- application/models/UserModels.php
+++ application/models/UserModels.php
@@ -1,1 +1,8 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class UserModels extends CI_Model {
+
+    public function get_all() {
+        return $this->db->order_by('created_at', 'DESC')->get('users')->result();
+    }
+}
