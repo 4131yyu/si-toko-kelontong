1e3d78bc88d7e11936dac903d75783216ce9ae48
Abiyyu <abiyyu18@gmail.com>
2026-06-24 13:03:12 +0800

    feat: tambah tampilan struk belanja

---
+++ application/views/transaksi/struk.php
@@ -0,0 +1,12 @@
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <div class="card shadow-sm">
+            <div class="card-header bg-success text-white text-center">
+                <i class="bi bi-check-circle"></i> Transaksi Berhasil!
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+</div>
