4758ff9dec7b9332a58095ac2f5022fbc4f7dddd
Feby <fbyy05@gmail.com>
2026-06-25 01:30:36 +0800

    Menambahkan deteksi halaman yang sedang diakses

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,2 +1,4 @@
 <?php
 $role = $this->session->userdata('role');
+$current_controller = strtolower($this->uri->segment(1) ?: 'dashboard');
+$current_method = strtolower($this->uri->segment(2) ?: 'index');
