5f171b4b745937bed6a6ce9002f5f0b75f48b696
Earlene <kimlenn427@gmail.com>
2026-06-24 22:56:49 +0800

    menambahkan function get by id

--- application/models/UserModels.php
+++ application/models/UserModels.php
@@ -5,4 +5,7 @@ class UserModels extends CI_Model {
     public function get_all() {
         return $this->db->order_by('created_at', 'DESC')->get('users')->result();
     }
+    public function get_by_id($id) {
+        return $this->db->get_where('users', ['id_user' => $id])->row();
+    }
 }
