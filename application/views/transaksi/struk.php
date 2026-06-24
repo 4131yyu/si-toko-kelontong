5f5f702d0394e90d36fba28bd3ffed4f830fac45
Abiyyu <abiyyu18@gmail.com>
2026-06-24 13:09:58 +0800

    menampilkan table kode transaksi,tanggal dan kasir

--- application/views/transaksi/struk.php
+++ application/views/transaksi/struk.php
@@ -4,8 +4,21 @@
             <div class="card-header bg-success text-white text-center">
                 <i class="bi bi-check-circle"></i> Transaksi Berhasil!
             </div>
-            <div>
-                
+            <div class="card-body">
+                <table class="table table-sm table-borderless mb-3">
+                    <tr>
+                        <th style="width:140px;">Kode Transaksi</th>
+                        <td>: <?= $transaksi->kode_transaksi ?></td>
+                    </tr>
+                    <tr>
+                        <th>Tanggal</th>
+                        <td>: <?= date('d-m-Y H:i', strtotime($transaksi->tgl_transaksi)) ?></td>
+                    </tr>
+                    <tr>
+                        <th>Kasir</th>
+                        <td>: <?= $transaksi->nama_lengkap ?></td>
+                    </tr>
+                </table>
             </div>
         </div>
     </div>
