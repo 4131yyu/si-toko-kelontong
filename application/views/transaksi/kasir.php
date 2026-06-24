79d47e54db54289e3ec3e94e2fde3254c688d08e
Abiyyu <abiyyu18@gmail.com>
2026-06-24 12:55:41 +0800

    feat: tambah form transaksi penjualan

---
+++ application/views/transaksi/kasir.php
@@ -0,0 +1,12 @@
+<div class="card shadow-sm">
+    <div class="card-header bg-white">
+        Form Transaksi Penjualan
+    </div>
+    <div class="card-body">
+        <form action="<?= base_url('Transaksi/proses') ?>" method="post" id="form-transaksi">
+            
+            
+        </form>
+    </div>
+</div>
+
