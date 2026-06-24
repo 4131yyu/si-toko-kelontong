96fe7fd9ed017b6cd30c40a46b612618ea248b58
Feby <fbyy05@gmail.com>
2026-06-25 01:14:49 +0800

    Menambahkan navbar Bootstrap dan struktur awal layout halaman

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,3 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
     <link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
     <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet">
+    </head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <div class="container-fluid">
