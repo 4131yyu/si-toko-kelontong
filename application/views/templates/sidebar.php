4532f89c8251d20f45156272409148a9c7ca0c93
Feby <fbyy05@gmail.com>
2026-06-26 00:46:12 +0800

    ganti warna

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -19,7 +19,7 @@ if (!function_exists('sidebar_active')) {
     }
 }
 ?>
-<nav class="sidebar bg-secondary" style="width:240px; min-height:calc(100vh - 56px);">
+<nav class="sidebar bg-dark" style="width:240px; min-height:calc(100vh - 56px);">
     <div class="d-flex flex-column p-3">
         <ul class="nav nav-pills flex-column mb-auto">
             <li class="nav-item">
