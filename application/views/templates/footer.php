b9eb7ce04f732df049661e42a07ac970d2d7252d
Abiyyu <abiyyu18@gmail.com>
2026-06-25 19:20:20 +0800

    feat: tambah link untuk jquery dan transaksi.js

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,6 +4,7 @@
 </main>
 </div>
 <script src="<?= base_url('assets/js/bootstrap.bundle.min.js') ?>"></script>
-<script src="<?= base_url('assets/js/jquery.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/jquery-3.7.1.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/transaksi.js?v=20260625') ?>"></script>
 </body>
 </html>
