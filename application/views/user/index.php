425089be3bf7984dd96fea27eae72fbcc6f7f0c3
Amru <bloodmon136@gmail.com>
2026-06-25 21:01:39 +0800

    index.php

---
+++ application/views/user/index.php
@@ -0,0 +1,7 @@
+<div class="card shadow-sm">
+    <div class="card-header bg-white d-flex justify-content-between align-items-center">
+        <span>Manajemen User</span>
+        <a href="<?= base_url('User/tambah') ?>" class="btn btn-primary btn-sm">
+            <i class="bi bi-plus-lg"></i> Tambah User
+        </a>
+    </div>
