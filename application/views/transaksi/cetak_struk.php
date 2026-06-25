8e7f3974536f2e0df8311b0f607bb66b8e131012
Abiyyu <abiyyu18@gmail.com>
2026-06-25 13:16:57 +0800

    feat: tambah view cetak struk

---
+++ application/views/transaksi/cetak_struk.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Struk Transaksi</title>
+    <link rel="stylesheet"href="<?= base_url('assets/css/bootstrap.min.css') ?>">
+</head>
+<body>
+
+<?php $this->load->view('transaksi/struk'); ?>
+
+<script>
+window.print();
+</script>
+
+</body>
+</html>
