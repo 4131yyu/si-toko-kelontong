0fbe9cd2ea4e6ccb29509e68511618c2c0e56815
Amru <bloodmon136@gmail.com>
2026-06-25 20:26:27 +0800

    menambahkan div class edit user dan tambah user

--- application/views/user/form.php
+++ application/views/user/form.php
@@ -1,1 +1,5 @@
 <?php $is_edit = isset($user) && $user; ?>
+<div class="card shadow-sm" style="max-width:600px;">
+    <div class="card-header bg-white">
+        <?= $is_edit ? 'Edit User' : 'Tambah User' ?>
+    </div>
