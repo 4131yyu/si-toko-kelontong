5cae27916991d6a45c18be0f590d20401849f2f3
Amru <bloodmon136@gmail.com>
2026-06-26 14:03:18 +0800

    menambahkan !

--- application/controllers/Kategori.php
+++ application/controllers/Kategori.php
@@ -4,7 +4,7 @@ class Kategori extends CI_Controller {
 
     public function __construct(){
         parent::__construct();
-        if ($this->session->userdata('logged_in')) redirect('Auth');
+        if (!$this->session->userdata('logged_in')) redirect('Auth');
         if ($this->session->userdata('role') !== 'admin') redirect('Dashboard');
         $this->load->model('KategoriModels');
     }
